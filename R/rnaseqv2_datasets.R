9d5194ca9ae6d1e81f68f9755f81043111de237f
James Hester <jhester@fredhutch.org>
2015-09-14 14:34:39 +0000

    Add RTCGA.clinical, RTCGA.mutations, RTCGA.rnaseq
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/RTCGA.rnaseq@3404 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/rnaseqv2_datasets.R
@@ -0,0 +1,183 @@
+#' 
+#' @title rnaseq datasets from TCGA project
+#'
+#' @description rnaseq data 
+#' format is explained here \url{https://wiki.nci.nih.gov/display/TCGA/RNASeq+Version+2}. Package provides rnaseq datasets from The Cancer Genome Atlas Project for all cohorts types from \url{http://gdac.broadinstitute.org/}.
+#' Data were downloaded using \link{RTCGA-package} and contain snapshots for the date: \code{2015-06-01}. The process is described here: \url{http://mi2-warsaw.github.io/RTCGA.data/}.
+#' Use cases, examples and information about datasets in \pkg{RTCGA.data} family can be found here: \code{browseVignettes("RTCGA")}
+#' 
+#' @details \code{browseVignettes("RTCGA")}
+#' 
+#' @return Data frames with rnaseq data.
+#' 
+#' @format NULL
+#' @source \url{http://gdac.broadinstitute.org/}
+#' 
+#' @examples browseVignettes("RTCGA")
+#' 
+#' @import RTCGA
+#' @name rnaseq
+#' @rdname rnaseq
+#' @aliases ACC.rnaseq,BLCA.rnaseq,BRCA.rnaseq,CESC.rnaseq,CHOL.rnaseq,COAD.rnaseq,DLBC.rnaseq,ESCA.rnaseq,GBM.rnaseq,HNSC.rnaseq,KICH.rnaseq,KIPAN.rnaseq,KIRC.rnaseq,KIRP.rnaseq,LAML.rnaseq,LGG.rnaseq,LIHC.rnaseq,LUAD.rnaseq,LUSC.rnaseq,MESO.rnaseq,OV.rnaseq,PAAD.rnaseq,PCPG.rnaseq,PRAD.rnaseq,READ.rnaseq,SKCM.rnaseq,STES.rnaseq,TGCT.rnaseq,THCA.rnaseq,THYM.rnaseq,UCEC.rnaseq,UCS.rnaseq,UVM.rnaseq
+#'
+"ACC.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#'
+"BLCA.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"BRCA.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"CESC.rnaseq"    
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"CHOL.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"COAD.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"DLBC.rnaseq"    
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"ESCA.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"GBM.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"HNSC.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"KICH.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"KIPAN.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"KIRC.rnaseq"    
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"KIRP.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"LAML.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"LGG.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"LIHC.rnaseq"    
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"LUAD.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"LUSC.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"MESO.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"OV.rnaseq"      
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"PAAD.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"PCPG.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"PRAD.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"READ.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"SKCM.rnaseq"  
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"STES.rnaseq"   
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"TGCT.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"THCA.rnaseq"    
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"THYM.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"UCEC.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"UCS.rnaseq"
+#' @name rnaseq
+#' @rdname rnaseq
+#' @format NULL
+#' 
+"UVM.rnaseq"
