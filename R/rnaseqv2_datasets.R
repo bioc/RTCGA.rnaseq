2eca51ee235027ca7e96c306b8d11b73e61c0537
Marcin Kosinski <m.p.kosinski@gmail.com>
2015-11-16 22:21:11 +0000

    update vignette, description, news and new versioning convention
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/RTCGA.rnaseq@3497 db2202da-8704-0410-b924-b125b3cd84ef

--- R/rnaseqv2_datasets.R
+++ R/rnaseqv2_datasets.R
@@ -4,7 +4,7 @@
 #' @description rnaseq data 
 #' format is explained here \url{https://wiki.nci.nih.gov/display/TCGA/RNASeq+Version+2}. Package provides rnaseq datasets from The Cancer Genome Atlas Project for all cohorts types from \url{http://gdac.broadinstitute.org/}.
 #' Data were downloaded using \link{RTCGA-package} and contain snapshots for the date: \code{2015-08-21}. The process is described here: \url{http://mi2-warsaw.github.io/RTCGA.data/}.
-#' Use cases, examples and information about datasets in \pkg{RTCGA.data} family can be found here: \code{browseVignettes("RTCGA")}
+#' Use cases, examples and information about datasets in \pkg{RTCGA.data} family can be found here: \code{browseVignettes("RTCGA")}. Data source is illumina hiseq Level 3 RSEM normalized expression data.
 #' 
 #' @details \code{browseVignettes("RTCGA")}
 #' 
